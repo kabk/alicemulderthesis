c8a095d069de55ccea6adbbc19e090422c5b0848
Alice Mulder <hallo@alicemulder.nl>
2016-03-03 13:06:52 +0100

    Nieuwste versie website
    Nieuwe lay-out, mintgroene stijl, verticaal scrollen

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
 $(function() {
-    $('.cover_side').css({'left':  $('#cover').outerWidth() - (25 + 40) + 'px'});
 
-    $('#start_reading').on('click', function() {
-        $('#container').animate({'left': '-123vw'});
+    positionFootnotes();
 
-        if ($('nav').hasClass('open')) {
-            $('nav').removeClass('open');
+    // REMOVE RETURN TO ARTICLE LINKS
+    $('a[title="return to article"]').remove();
+
+    // CONVERT URL FOOTNOTES TO LINKS
+    $('.footnote').each(function() {
+        var child = $(this).children('p')[0],
+            url = child.innerHTML,
+            protocol = url.slice(0, 7);
+
+        if (protocol === 'http://') {
+            $(child).wrapInner('<a href="' +url+ '" target="_blank"></a>');
         }
     });
 
-
-    $('.content_side').on('click', function() {
-        if ($('nav').hasClass('open')) {
-            $('#container').animate({'left': '-123vw'});
-            $('nav').removeClass('open');
-
-        }
-        else {
-            $('#container').animate({'left': '-100vw'});
-            $('nav').addClass('open');
-        }
-
+    $('#start_reading').on('click', function(event) {
+        $('body, html').css('overflow', 'auto');
+        $('body').scrollTo($(this).attr('data-target'), 'slow');
     });
 
-
-    $('.cover_side').on('click', function() {
-
-        if ($('nav').hasClass('open')) {
-            $('#container').animate({'left': '0'});
-            $('nav').removeClass('open');
-
-        }
-        else {
-            $('#container').animate({'left': '-23vw'});
-            $('nav').addClass('open');
-        }
-
+    $('.menu li').on('click', function(event) {
+        $('body').scrollTo($(this).attr('data-target'), 'slow', function() {
+            $('.menu').animate({'left': '-300px'});
+            $('.menu_toggle').animate({
+                'left': '25px'
+            }).html('OPEN');
+        });
     });
 
-    $('.spine').on('click', function() {
-        var $content = $(this).siblings('.content');
+    $('.menu_toggle').on('click', function(event) {
+        $('body, html').css('overflow', 'scroll');
 
-        if ($content.hasClass('sesam')) {
-            $content.css({'display': 'none'}).removeClass('sesam');
+        $(this).toggleClass('open');
+
+        if (!$(this).hasClass('open')) {
+            $('.menu').animate({'left': '-300px'});
+            $(this).animate({
+                'left': '25px'
+            }).html('OPEN');
         }
         else {
-            $content.css({'display': 'block'}).addClass('sesam');
+            $('.menu').animate({'left': '0px'});
+            $(this).animate({
+                'left': '325px'
+            }).html('SLUIT');
         }
+    })
+
+    if ($('body').scrollTop() !== 0 ) {
+        $('body, html').css('overflow', 'scroll');
+    }
+
+    $('.print_toggle').on('click', function() {
+        window.open(''); // link to pdf
     });
 });
+
+function positionFootnotes() {
+    // POSITION ALL FOOTNOTES PROPERLY
+    $('a[rel="footnote"]').each(function() {
+        var fnId = $(this).attr('href'),
+            fnPosTop = $(this).offset().top,
+            detached = $(fnId).detach();
+
+        $(this).append(detached);
+    });
+}
